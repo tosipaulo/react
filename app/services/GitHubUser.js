e3c61881776f68d1af980a63ed7e70e8420740ff
Paulo Tosi <tosi.paulo@gmail.com>
2017-06-03 22:15:37 -0300

    Finalizando aplicação get github es5

---
+++ app/services/GitHubUser.js
@@ -0,0 +1,15 @@
+var axios = require('axios');
+
+var GitHubUser = {
+
+	getByUserName: function(username) {
+		return axios.get('https://api.github.com/users/' + username);
+	},
+
+	getReposByUsername: function(username) {
+		return axios.get('https://api.github.com/users/' + username + '/repos');
+	}
+
+};
+
+module.exports = GitHubUser;
